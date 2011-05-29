243af6e42977108ac47365856bef9affa3df4776
Adel <benflen@gmail.com>
2011-05-29 16:10:48 +0200

    completing the web services stubs

--- HyperPathServer/HyperPathServer-ejb/src/java/org/hyperpath/services/emails/EmailsServices.java
+++ HyperPathServer/HyperPathServer-ejb/src/java/org/hyperpath/services/emails/EmailsServices.java
@@ -4,6 +4,7 @@
  */
 package org.hyperpath.services.emails;
 
+import java.util.List;
 import javax.annotation.Resource;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
@@ -12,6 +13,10 @@ import javax.ejb.Stateless;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceUnit;
 import javax.transaction.UserTransaction;
+import org.hyperpath.persistence.entities.Emails;
+import org.hyperpath.persistence.jpa.exceptions.NonexistentEntityException;
+import org.hyperpath.persistence.jpa.exceptions.PreexistingEntityException;
+import org.hyperpath.persistence.jpa.exceptions.RollbackFailureException;
 
 /**
  *
@@ -25,9 +30,38 @@ public class EmailsServices {
 
     @PersistenceUnit
     EntityManagerFactory emf;
-    /** This is a sample web service operation */
-    @WebMethod(operationName = "hello")
-    public String hello(@WebParam(name = "name") String txt) {
-        return "Hello " + txt + " !";
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "addEmail")
+    public void addEmail(@WebParam(name = "email")
+    Emails email) throws Exception, PreexistingEntityException, RollbackFailureException {
+    }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "updateEmail")
+    public void updateEmail(@WebParam(name = "email")
+    Emails email) throws Exception, NonexistentEntityException, RollbackFailureException {
+    }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "deleteEmail")
+    public void deleteEmail(@WebParam(name = "emailId")
+    Integer emailId) throws Exception, NonexistentEntityException, RollbackFailureException {
+    }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "findEmail")
+    public List<Emails> findEmail(@WebParam(name = "email")
+    String email) throws Exception, NonexistentEntityException, RollbackFailureException {
+        //TODO write your implementation code here:
+        return null;
     }
 }
