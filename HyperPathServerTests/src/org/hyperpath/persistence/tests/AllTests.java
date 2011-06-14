b1c4b3f8bcbd1ae45ccbdc96e85914cb3c884350
chedi <chedi@localhost.localdomain>
2011-06-14 13:19:28 +0100

    Adding new test project for the server application: * initial project structure * initial test suite * mail service test case

---
+++ HyperPathServerTests/src/org/hyperpath/persistence/tests/AllTests.java
@@ -0,0 +1,14 @@
+package org.hyperpath.persistence.tests;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class AllTests {
+
+	public static Test suite() {
+		TestSuite suite = new TestSuite(AllTests.class.getName());
+		suite.addTestSuite(MailsTests.class);
+		return suite;
+	}
+
+}
