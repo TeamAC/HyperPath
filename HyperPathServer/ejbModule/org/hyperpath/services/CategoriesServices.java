4b14ec3c1d84e0e67c5fc05000f3a2b7fc851dbb
chedi <chedi.toueiti@gmail.com>
2011-06-14 17:49:12 +0100

    Minor synch update

--- HyperPathServer/ejbModule/org/hyperpath/services/CategoriesServices.java
+++ HyperPathServer/ejbModule/org/hyperpath/services/CategoriesServices.java
@@ -1,8 +1,6 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-package org.hyperpath.services.categories;
+package org.hyperpath.services;
+
+import java.util.List;
 
 import javax.annotation.Resource;
 import javax.jws.WebService;
@@ -10,19 +8,15 @@ import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import javax.persistence.PersistenceUnit;
 import javax.transaction.UserTransaction;
 import org.hyperpath.persistence.entities.Categories;
-import org.hyperpath.persistence.entities.Services;
+import org.hyperpath.persistence.jpa.CategoriesJpaController;
 import org.hyperpath.persistence.jpa.exceptions.NonexistentEntityException;
 import org.hyperpath.persistence.jpa.exceptions.PreexistingEntityException;
 import org.hyperpath.persistence.jpa.exceptions.RollbackFailureException;
-import org.jboss.weld.logging.Category;
 
-/**
- * 
- * @author adel
- */
 @WebService(serviceName = "CategoriesServices")
 @Stateless()
 public class CategoriesServices {
@@ -32,52 +26,115 @@ public class CategoriesServices {
   @PersistenceUnit
   EntityManagerFactory    emf;
 
+  CategoriesJpaController controller;
+
+  /**
+   * Add new category
+   */
   @WebMethod(operationName = "addCategory")
-  public void addService(@WebParam(name = "category") Category service)
+  public void addService(@WebParam(name = "category") Categories category)
       throws Exception, PreexistingEntityException,
       RollbackFailureException {
+    emf = Persistence.createEntityManagerFactory("HyperPathServerPU");
+    controller = new CategoriesJpaController(utx, emf);
+    controller.create(category);
   }
 
   /**
-   * Web service operation
+   * Update category
    */
   @WebMethod(operationName = "updateCategory")
   public void updateCategory(@WebParam(name = "category") Categories category)
       throws Exception, NonexistentEntityException,
       RollbackFailureException {
+    emf = Persistence.createEntityManagerFactory("HyperPathServerPU");
+    controller = new CategoriesJpaController(utx, emf);
+    controller.edit(category);
   }
 
   /**
-   * Web service operation
+   * Delete category
    */
   @WebMethod(operationName = "deleteCategory")
   public void deleteCategory(@WebParam(name = "categoryId") Integer categoryId)
       throws Exception, NonexistentEntityException,
       RollbackFailureException {
+    emf = Persistence.createEntityManagerFactory("HyperPathServerPU");
+    controller = new CategoriesJpaController(utx, emf);
+    controller.destroy(categoryId);
   }
 
   /**
-   * Web service operation
+   * Find category by exact label
    */
-  @WebMethod(operationName = "findCategoryByLabel")
-  public Categories findCategoryByLabel(
-                                        @WebParam(name = "categoryLabel") String categoryLabel)
+  @WebMethod(operationName = "findCategoryByExactLabel")
+  public List<Categories> findCategoryByExactLabel(@WebParam(name = "categoryLabel") String categoryLabel)
       throws Exception, NonexistentEntityException,
       RollbackFailureException {
-    // TODO write your implementation code here:
-    return null;
+    emf = Persistence.createEntityManagerFactory("HyperPathServerPU");
+    controller = new CategoriesJpaController(utx, emf);
+    return controller.findCategoriesByExacLabel(categoryLabel);
   }
 
   /**
-   * Web service operation
+   * Find category by approximate label
    */
-  @WebMethod(operationName = "findCategoryByService")
-  public Categories findCategoryByService(
-                                          @WebParam(name = "service") Services service)
+  @WebMethod(operationName = "findCategoriesByApproximateLabel")
+  public List<Categories> findCategoriesByApproximateLabel(@WebParam(name = "categoryLabel") String categoryLabel)
+      throws Exception, NonexistentEntityException,
+      RollbackFailureException {
+    emf = Persistence.createEntityManagerFactory("HyperPathServerPU");
+    controller = new CategoriesJpaController(utx, emf);
+    return controller.findCategoriesByApproximateLabel(categoryLabel);
+  }
+
+  /**
+   * Find category by exact description
+   */
+  @WebMethod(operationName = "findCategoriesByExactDescription")
+  public List<Categories> findCategoriesByExactDescription(@WebParam(name = "categoryLabel") String categoryDescription)
+      throws Exception, NonexistentEntityException,
+      RollbackFailureException {
+    emf = Persistence.createEntityManagerFactory("HyperPathServerPU");
+    controller = new CategoriesJpaController(utx, emf);
+    return controller.findCategoriesByExactDescription(categoryDescription);
+  }
+
+  /**
+   * Find category by approximate description
+   */
+  @WebMethod(operationName = "findCategoriesByApproximateDescription")
+  public List<Categories> findCategoriesByApproximateDescription(@WebParam(name = "categoryLabel") String categoryDescription)
+      throws Exception, NonexistentEntityException,
+      RollbackFailureException {
+    emf = Persistence.createEntityManagerFactory("HyperPathServerPU");
+    controller = new CategoriesJpaController(utx, emf);
+    return controller.findCategoriesByApproximateDescription(categoryDescription);
+  }
+
+  /**
+   * Find category by approximate description
+   */
+  @WebMethod(operationName = "listAllCategories")
+  public List<Categories> listAllCategories()
     throws Exception,
       NonexistentEntityException,
       RollbackFailureException {
-    // TODO write your implementation code here:
-    return null;
+    emf = Persistence.createEntityManagerFactory("HyperPathServerPU");
+    controller = new CategoriesJpaController(utx, emf);
+    return controller.findCategoriesEntities();
+  }
+
+  /**
+   * Find total categories number
+   */
+  @WebMethod(operationName = "countCategories")
+  public int countCategories()
+    throws Exception,
+      NonexistentEntityException,
+      RollbackFailureException {
+    emf = Persistence.createEntityManagerFactory("HyperPathServerPU");
+    controller = new CategoriesJpaController(utx, emf);
+    return controller.getCategoriesCount();
   }
 }
