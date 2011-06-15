f5b3f75d6218fc4c2ebd77ab0936ed6ebfb34960
chedi <chedi.toueiti@gmail.com>
2011-06-15 08:34:30 +0100

    88: Add webservice endpoint for approximate address search by category http://github.com/TeamAC/HyperPath/issues/issue/88
    89: Add webservice endpoint for address search by range
    http://github.com/TeamAC/HyperPath/issues/issue/89
    
    90: Add webservice endpoint for services search by address and
    category
    http://github.com/TeamAC/HyperPath/issues/issue/90
    
    91: Add webservice endpoint for advertizers search by address and
    category
    http://github.com/TeamAC/HyperPath/issues/issue/91
    
    92: Add webservice endpoint for clients search by address and category
    http://github.com/TeamAC/HyperPath/issues/issue/92

--- HyperPathServer/ejbModule/org/hyperpath/services/addresses/AddressesServices.java
+++ HyperPathServer/ejbModule/org/hyperpath/services/addresses/AddressesServices.java
@@ -6,11 +6,9 @@ import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.ejb.Stateless;
-import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.PersistenceUnit;
-import javax.persistence.Query;
 import javax.transaction.UserTransaction;
 import javax.xml.ws.RequestWrapper;
 import javax.xml.ws.ResponseWrapper;
@@ -20,7 +18,6 @@ import org.hyperpath.persistence.entities.Categories;
 import org.hyperpath.persistence.entities.Clients;
 import org.hyperpath.persistence.entities.Services;
 import org.hyperpath.persistence.jpa.AddressJpaController;
-import org.hyperpath.persistence.jpa.EmailsJpaController;
 import org.hyperpath.persistence.jpa.exceptions.NonexistentEntityException;
 import org.hyperpath.persistence.jpa.exceptions.PreexistingEntityException;
 import org.hyperpath.persistence.jpa.exceptions.RollbackFailureException;
@@ -34,6 +31,8 @@ public class AddressesServices {
   @PersistenceUnit
   EntityManagerFactory    emf;
 
+  AddressJpaController controller;
+
   /**
    * list all address
    */
@@ -41,7 +40,7 @@ public class AddressesServices {
   public List<Address> listAllAddress() throws Exception,
       RollbackFailureException, NonexistentEntityException {
     emf = Persistence.createEntityManagerFactory("HyperPathServerPU");
-    AddressJpaController controller = new AddressJpaController(utx, emf);
+    controller = new AddressJpaController(utx, emf);
     return controller.findAddressEntities();
   }
 
@@ -54,16 +53,7 @@ public class AddressesServices {
       PreexistingEntityException {
     emf = Persistence.createEntityManagerFactory("HyperPathServerPU");
     AddressJpaController controller = new AddressJpaController(utx, emf);
-    try {
-      if (controller.findAddress(address) == null) {
-        controller.create(address);
-      } else {
-        throw new PreexistingEntityException(
-            "Address already present in the database");
-      }
-    } catch (Exception e) {
-      throw e;
-    }
+    controller.create(address);
   }
 
   /**
@@ -73,17 +63,8 @@ public class AddressesServices {
   public void updateAddress(@WebParam(name = "address") Address address)
       throws Exception, RollbackFailureException,
       PreexistingEntityException {
-    AddressJpaController controller = new AddressJpaController(utx, emf);
-    try {
-      if (address.getId() != null) {
-        controller.edit(address);
-      } else {
-        throw new Exception(
-            "To update address the id field must be specified");
-      }
-    } catch (Exception e) {
-      throw e;
-    }
+    controller = new AddressJpaController(utx, emf);
+    controller.edit(address);
   }
 
   /**
@@ -93,40 +74,21 @@ public class AddressesServices {
   public void deleteAddress(@WebParam(name = "address") Address address)
       throws Exception, RollbackFailureException,
       NonexistentEntityException {
-    AddressJpaController controller = new AddressJpaController(utx, emf);
-    try {
-      Integer id;
-      if ((id = address.getId()) != null
-          || controller.findAddress(address) != null) {
-        controller.destroy(id);
-      } else {
-        throw new Exception(
-            "To delete address the id field must be specified or the address must match one in the database");
-      }
-    } catch (Exception e) {
-      throw e;
-    }
+    controller = new AddressJpaController(utx, emf);
+    controller.destroy(address.getId());
   }
 
   /**
    * Find address by textual approximation
    */
-  @WebMethod(operationName = "findAddress")
-  public Address findAddress(
+  @WebMethod(operationName = "findApproximateAddressesByCategory")
+  public List<Address> findApproximateAddressesByCategory(
                              @WebParam(name = "category") Categories category,
                              @WebParam(name = "address") String address)
     throws Exception,
       NonexistentEntityException {
-    AddressJpaController a_controller = new AddressJpaController(utx, emf);
-    try {
-      if (category.getId() != null) {
-        EntityManager em = emf.createEntityManager();
-        em.createNamedQuery(address);
-      }
-    } catch (Exception e) {
-      throw e;
-    }
-    return null;
+    controller = new AddressJpaController(utx, emf);
+    return controller.findApproximateAddressesByCategory(category, address);
   }
 
   /**
@@ -135,91 +97,45 @@ public class AddressesServices {
   @WebMethod(operationName = "findAddressByLocation")
   @RequestWrapper(className = "org.findAddressByLocation")
   @ResponseWrapper(className = "org.findAddressByLocationResponse")
-  public Address findAddressByLocation(
+  public List<Address> findAddressByLocation(
                                        @WebParam(name = "category") Categories category,
                                        @WebParam(name = "location") String location,
                                        @WebParam(name = "range") int range)
       throws NonexistentEntityException {
-    // TODO write your implementation code here:
-    return null;
+    controller = new AddressJpaController(utx, emf);
+    return controller.findAddressByRange(category, location, range);
   }
 
   /**
    * Find services based on their address
    */
-  @WebMethod(operationName = "findServiceByAddress")
-  public List<org.hyperpath.persistence.entities.Services> findServiceByAddress(
-                                                                                @WebParam(name = "address") Address address)
+  @WebMethod(operationName = "findServicesByAddress")
+  public List<Services> findServicesByAddress(@WebParam(name = "address") Address address)
     throws Exception,
       NonexistentEntityException {
-    try {
-      if (address.getId() == null)
-        throw new Exception(
-            "Address id must be initialiazed before using search");
-      EntityManager em = emf.createEntityManager();
-      Query qr = em.createNamedQuery("Address.findServiceByAddress");
-      qr.setParameter("addressId", address.getId());
-      List<Services> rs = qr.getResultList();
-      if (rs.isEmpty())
-        throw new NonexistentEntityException(
-            "There is no service in this address");
-      return rs;
-    } catch (Exception e) {
-      throw e;
-    }
+    controller = new AddressJpaController(utx, emf);
+    return controller.findServicesByAddress(address);
   }
 
   /**
    * Find advertisers base on their address
    */
-  @WebMethod(operationName = "findAdvertizerByAddress")
-  public List<org.hyperpath.persistence.entities.Advertisers> findAdvertizerByAddress(
-                                                                                      @WebParam(name = "address") Address address)
+  @WebMethod(operationName = "findAdvertizersByAddress")
+  public List<Advertisers> findAdvertizersByAddress(@WebParam(name = "address") Address address)
     throws Exception,
       NonexistentEntityException {
-
-    try {
-      if (address.getId() == null)
-        throw new Exception(
-            "Address id must be initialiazed before using search");
-      EntityManager em = emf.createEntityManager();
-      Query qr = em.createNamedQuery("Address.findAdvertiserByAddress");
-      qr.setParameter("addressId", address.getId());
-      List<Advertisers> rs = qr.getResultList();
-      if (rs.isEmpty())
-        throw new NonexistentEntityException(
-            "There is no advertizers in this address");
-      return rs;
-    } catch (Exception e) {
-      throw e;
-    }
-
+    controller = new AddressJpaController(utx, emf);
+    return controller.findAdvertizersByAddress(address);
   }
 
   /**
    * Find clients based on their address
    */
-  @WebMethod(operationName = "findClientByAddress")
-  public List<Clients> findClientByAddress(
-                                           @WebParam(name = "address") Address address)
+  @WebMethod(operationName = "findClientsByAddress")
+  public List<Clients> findClientsByAddress(@WebParam(name = "address") Address address)
     throws Exception,
       NonexistentEntityException {
-    try {
-      if (address.getId() == null) {
-        throw new Exception(
-            "Address id must be initialiazed before using search");
-      }
-      EntityManager em = emf.createEntityManager();
-      Query qr = em.createNamedQuery("Address.findClientByAddress");
-      qr.setParameter("clientId", address.getId());
-      List<Clients> result = qr.getResultList();
-      if (result.isEmpty()) {
-        throw new NonexistentEntityException(
-            "There is no client living at this address");
-      }
-      return result;
-    } catch (Exception e) {
-      throw e;
-    }
+    controller = new AddressJpaController(utx, emf);
+    return controller.findClientsByAddress(address);
   }
 }
