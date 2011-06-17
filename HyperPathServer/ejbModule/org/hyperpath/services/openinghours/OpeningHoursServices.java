1e7220331eca0e3aa5468eab76286f60d991bae7
chedi <chedi.toueiti@gmail.com>
2011-06-17 03:20:23 +0100

    133: Implement Opening hours webservice addOpeningHours http://github.com/TeamAC/HyperPath/issues/issue/133
    134: Implement Opening hours webservice deleteOpeningHours
    http://github.com/TeamAC/HyperPath/issues/issue/134
    
    135: Implement Opening hours webservice updateOpeningHours
    http://github.com/TeamAC/HyperPath/issues/issue/135

--- HyperPathServer/ejbModule/org/hyperpath/services/openinghours/OpeningHoursServices.java
+++ HyperPathServer/ejbModule/org/hyperpath/services/openinghours/OpeningHoursServices.java
@@ -1,26 +1,19 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.hyperpath.services.openinghours;
 
 import javax.annotation.Resource;
-import javax.jws.Oneway;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import javax.persistence.PersistenceUnit;
 import javax.transaction.UserTransaction;
 import org.hyperpath.persistence.entities.OpeningHours;
+import org.hyperpath.persistence.jpa.OpeningHoursJpaController;
 import org.hyperpath.persistence.jpa.exceptions.NonexistentEntityException;
 import org.hyperpath.persistence.jpa.exceptions.RollbackFailureException;
 
-/**
- * 
- * @author adel
- */
 @WebService(serviceName = "OpeningHoursServices")
 @Stateless()
 public class OpeningHoursServices {
@@ -30,34 +23,50 @@ public class OpeningHoursServices {
   @PersistenceUnit
   EntityManagerFactory    emf;
 
+  OpeningHoursJpaController controller;
   /**
-   * Web service operation
+   * Add new opening hour
    */
   @WebMethod(operationName = "addOpeningHours")
-  public void addOpeningHours(
-                              @WebParam(name = "openingHours") OpeningHours openingHours)
-      throws Exception, NonexistentEntityException,
-      RollbackFailureException {
+  public void addOpeningHours(@WebParam(name = "openingHours") OpeningHours openingHours)
+    throws
+    Exception,
+    NonexistentEntityException,
+    RollbackFailureException
+  {
+    emf = Persistence.createEntityManagerFactory("HyperPathServerPU");
+    controller = new OpeningHoursJpaController(utx, emf);
+    controller.create(openingHours);
   }
 
   /**
-   * Web service operation
+   * Delete opening hour
    */
   @WebMethod(operationName = "deleteOpeningHours")
-  public void deleteOpeningHours(
-                                 @WebParam(name = "openingHoursId") Integer openingHoursId)
-      throws Exception, NonexistentEntityException,
-      RollbackFailureException {
+  public void deleteOpeningHours(@WebParam(name = "openingHoursId") Integer openingHoursId)
+    throws
+    Exception,
+    NonexistentEntityException,
+    RollbackFailureException
+  {
+    emf = Persistence.createEntityManagerFactory("HyperPathServerPU");
+    controller = new OpeningHoursJpaController(utx, emf);
+    controller.destroy(openingHoursId);
   }
 
   /**
-   * Web service operation
+   * Update opening hour
    */
   @WebMethod(operationName = "updateOpeningHours")
-  public void updateOpeningHours(
-                                 @WebParam(name = "openingHoursId") Integer openingHoursId)
-      throws Exception, NonexistentEntityException,
-      RollbackFailureException {
+  public void updateOpeningHours(@WebParam(name = "openingHoursId") Integer openingHoursId)
+    throws
+    Exception,
+    NonexistentEntityException,
+    RollbackFailureException
+  {
+    emf = Persistence.createEntityManagerFactory("HyperPathServerPU");
+    controller = new OpeningHoursJpaController(utx, emf);
+    controller.destroy(openingHoursId);
   }
 
 }
